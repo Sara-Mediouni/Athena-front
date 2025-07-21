c195794b5d554b8c348f2e0311d69b06336429df
unknown <mediounisarra99@gmail.com>
2025-07-21 07:45:10 +0100

    role guard added

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,20 +1,50 @@
+// auth-role.guard.ts
 import { inject } from '@angular/core';
-import { CanActivateFn, Router, UrlTree } from '@angular/router';
+import {
+  CanActivateChildFn,
+  Router,
+  UrlTree
+} from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 import { LoaderService } from '../apps/loader/loader.service';
+import { UserService } from '../Service/UserService';
 
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authRoleGuard: CanActivateChildFn = async (route, state): Promise<boolean | UrlTree> => {
   const router = inject(Router);
   const loader = inject(LoaderService);
+  const userService = inject(UserService);
 
-  const token = (typeof localStorage !== "undefined") ? localStorage.getItem('accessToken'): null;
+  const token = (typeof localStorage !== 'undefined') ? localStorage.getItem('accessToken') : null;
 
-  if (token) {
-    loader.hide();
-    return true;
-  } else {
+  if (!token) {
     loader.show();
-    // Retourner un UrlTree pour rediriger sans flash
     return router.createUrlTree(['/authentication']);
   }
+
+  let user;
+  try {
+    user = await firstValueFrom(userService.getUserConnected(token));
+  } catch (e) {
+    console.error('Erreur utilisateur :', e);
+    return router.createUrlTree(['/authentication']);
+  }
+
+  const userRole = user?.role || 'USER';
+  const allowedRoles = route.data?.['roles'] as string[];
+
+  if (!allowedRoles || allowedRoles.includes(userRole)) {
+    loader.hide();
+    return true;
+  }
+
+  loader.hide();
+  return redirectByRole(userRole, router);
 };
+
+function redirectByRole(role: string, router: Router): UrlTree {
+  if (role === 'USER') {
+    return router.createUrlTree(['/blank-page']);
+  }
+  return router.createUrlTree(['/dashboard']);
+}
